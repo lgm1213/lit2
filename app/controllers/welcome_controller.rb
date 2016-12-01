65e21fd048e9dc16c3d9ac2d7b805030c57ef5cf
LGM1213 <lgm1213@gmail.com>
2016-12-01 18:33:16 -0500

    api tools

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,16 @@
 class WelcomeController < ApplicationController
+		include YelpTools
 	def index
-		client = Yelp::Client.new({
-			consumer_key: ENV["CONSUMER_KEY"],
-			consumer_secret: ENV["CONSUMER_SECRET"],
-  		token: ENV["TOKEN"],
-  		token_secret: ENV["TOKEN_SECRET"]
-		})
-  	parameters = { term: "Nightlife", limit: 3 }
-    @profile = client.search('33127', parameters)
+		YelpTools.client
+		@profile=YelpTools.search('33127', { term: "Nightlife", limit: 3 })
+		
+		# client = Yelp::Client.new({
+		# 	consumer_key: ENV["CONSUMER_KEY"],
+		# 	consumer_secret: ENV["CONSUMER_SECRET"],
+  # 		token: ENV["TOKEN"],
+  # 		token_secret: ENV["TOKEN_SECRET"]
+		# })
+  # 	parameters = { term: "Nightlife", limit: 3 }
+    # @profile = client.search('33127', parameters)
   end
 end
--- app/models/yelp_tools.rb
+++ app/models/yelp_tools.rb
@@ -1,15 +1,17 @@
-class Yelp_Tools < ApplicationController
-	def self.client
-		 @@client ||= Yelp::Client.new({
-      consumer_key: ENV["CONSUMER_KEY"],
-      consumer_secret: ENV["CONSUMER_SECRET"],
-      token: ENV["TOKEN"],
-      token_secret: ENV["TOKEN_SECRET"]
-    })
-	end
+module YelpTools
+	class YelpTools < ApplicationController
+		def self.client
+			 @@client ||= Yelp::Client.new({
+	      consumer_key: ENV["CONSUMER_KEY"],
+	      consumer_secret: ENV["CONSUMER_SECRET"],
+	      token: ENV["TOKEN"],
+	      token_secret: ENV["TOKEN_SECRET"]
+	    })
+		end
 
-	def self.search(zip,options)
-		client.search(zip, options)
-	end
+		def self.search(zip,options)
+			client.search(zip, options)
+		end
 
+	end
 end
