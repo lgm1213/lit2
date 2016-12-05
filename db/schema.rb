b605619f9fefab0fb9df1338edcb06147d9a1396
LGM1213 <lgm1213@gmail.com>
2016-12-04 23:35:04 -0500

    linked profile link to user/:id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204221326) do
+ActiveRecord::Schema.define(version: 20161205040631) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
