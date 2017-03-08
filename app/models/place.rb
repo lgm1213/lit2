0840dbe2b848bba18fcc9e60e1e97dea7b328bcb
LGM1213 <lgm1213@gmail.com>
2017-03-08 16:45:29 -0500

    added materialize css cards and icons

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,5 @@
 class Place < ApplicationRecord
 	# include Geocoder
-	ratyrate_rateable 'atmosphere', 'drinks', 'wait_time', 'cost', 'music'
 	has_many :ratings
   has_many :comment
   validates :yelp_id, uniqueness: true
